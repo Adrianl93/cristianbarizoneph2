6abce3731111b1ff760470650e5bd98210b5ff89
Adrianl93 <[200~laborde.adrian2012@gmail.comgit config --global user.email laborde.adrian2012@gmail.com>
2024-10-17 17:25:35 -0300

    Footer responsive with text adaptation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Footer.css';
-import whatsappIcon from '../assets/whatsappIcon.svg'
-import instagramIcon from '../assets/instagramIcon.svg'
-import emailIcon from '../assets/emailIcon.svg'
+import whatsappIcon from '../assets/whatsappIcon.svg';
+import instagramIcon from '../assets/instagramIcon.svg';
+import emailIcon from '../assets/emailIcon.svg';
 
 const Footer = ({ data }) => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    // Función para manejar el tamaño de la pantalla
+    const handleResize = () => {
+        setIsMobile(window.innerWidth <= 768); // Detecta si el ancho es menor o igual a 768px
+    };
+
+    useEffect(() => {
+        // Añadir un listener para cuando cambie el tamaño de la ventana
+        window.addEventListener('resize', handleResize);
+
+        // Llamamos a handleResize al cargar el componente
+        handleResize();
+
+        // Limpieza del listener al desmontar el componente
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <footer className="footer">
             <div className="footer-text">
-                Animate a comunicarte con nosotros a través de nuestras redes!!
+                {isMobile ? 'Seguinos en nuestras redes' : 'Animate a comunicarte con nosotros a través de nuestras redes!!'}
             </div>
             <a href={data.whatsapp} target="_blank" rel="noreferrer">
                 <img src={whatsappIcon} alt="WhatsApp Icon" width="25" height="25" />
