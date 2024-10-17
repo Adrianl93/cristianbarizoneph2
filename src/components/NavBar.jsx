f33a274e6ea9ac45346d75bd343068684c7c7f1d
Adrianl93 <[200~laborde.adrian2012@gmail.comgit config --global user.email laborde.adrian2012@gmail.com>
2024-10-17 17:18:26 -0300

    navBar responsive with Hamburger-Menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/NavBar.css';
 
 const NavBar = ({ data }) => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <nav className="navbar">
             <a href="/">Home</a>
-            {data.sections.map((section) => (
-                <a key={section.id} href={`/#${section.id}`}>{section.label}</a>
-            ))}
-            <a href="https://cristianbarizoneph.empretienda.com.ar/">Tienda Nube</a>
+
+            <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
+                {data.sections.map((section) => (
+                    <a key={section.id} href={`/#${section.id}`}>{section.label}</a>
+                ))}
+                <a href="https://cristianbarizoneph.empretienda.com.ar/">Tienda Nube</a>
+            </div>
+
+            <div className="hamburger" onClick={toggleMenu}>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+            </div>
         </nav>
     );
 };
