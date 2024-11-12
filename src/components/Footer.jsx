ac74fdb5b37e8e516ebdb1a4925b60187c747c1f
Adrianl93 <[200~laborde.adrian2012@gmail.comgit config --global user.email laborde.adrian2012@gmail.com>
2024-11-12 11:04:16 -0300

    gap to footer items

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,15 +28,19 @@ const Footer = ({ data }) => {
             <div className="footer-text">
                 {isMobile ? 'Seguinos en nuestras redes' : 'Animate a comunicarte con nosotros a través de nuestras redes!!'}
             </div>
-            <a href={data.whatsapp} target="_blank" rel="noreferrer">
-                <img src={whatsappIcon} alt="WhatsApp Icon" width="25" height="25" />
-            </a>
-            <a href={data.instagram} target="_blank" rel="noreferrer">
-                <img src={instagramIcon} alt="Instagram Icon" width="25" height="25" />
-            </a>
-            <a href={data.email} target="_blank" rel="noreferrer">
-                <img src={emailIcon} alt="Email Icon" width="25" height="25" />
-            </a>
+            <div className="footer-buttons"> 
+                <a href={data.whatsapp} target="_blank" rel="noreferrer">
+                    <img src={whatsappIcon} alt="WhatsApp Icon" width="25" height="25" />
+                </a>
+                <a href={data.instagram} target="_blank" rel="noreferrer">
+                    <img src={instagramIcon} alt="Instagram Icon" width="25" height="25" />
+                </a>
+                <a href={data.email} target="_blank" rel="noreferrer">
+                    <img src={emailIcon} alt="Email Icon" width="25" height="25" />
+                </a>
+
+            </div>
+            
         </footer>
     );
 };
